ad3cd9f1b33b07cd2e92fb800e10d168b3d7ebdf
PhilLavoie <maidenphil@hotmail.com>
2014-11-12 14:21:51 -0500

    Added some internal utility functions in protocol.rs in preparation for support of the rest of the native protocol extraction functions.

--- src/thincrust/protocol.rs
+++ src/thincrust/protocol.rs
@@ -3,9 +3,42 @@ use c::types::{c_int, protoent};
 
 use std::c_str::CString;
 
+fn extract_number(entry: *const protoent) -> c_int {
+  unsafe{ (*entry).p_proto }
+}
+
+fn extract_name(entry: *const protoent) -> String {
+  unsafe{ CString::new((*entry).p_name, false).as_str().unwrap().to_string() } 
+}
+
+fn extract_aliases(entry: *const protoent) -> Vec<String> {
+  let mut aliases: Vec<String> = Vec::new();
+  let mut current_alias = unsafe{ (*entry).p_aliases };
+    
+  while !current_alias.is_null() {
+    let rust_string = 
+      unsafe{ CString::new(*current_alias, false).as_str().unwrap().to_string() };
+      
+    aliases.push(rust_string);
+
+    current_alias = unsafe{ current_alias.offset(1) }; 
+  }
+
+  aliases
+}
+
+trait FromNative {
+  fn from_native(entry: *const protoent) -> Self;
+}
 
 pub struct Protocol {
   number: c_int
+}
+
+impl FromNative for Protocol {
+  fn from_native(entry: *const protoent) -> Protocol {
+    Protocol{ number: extract_number(entry) }
+  }
 }
 
 impl Protocol {
@@ -17,12 +50,11 @@ impl Protocol {
     }
 
     Ok( 
-      Protocol {
-        number: unsafe{ (*entry).p_proto }
-      }
+      FromNative::from_native(entry)
     )
   }
- 
+
+  pub fn default() -> Protocol { Protocol{ number: 0 } }
 }
 
 pub struct ProtocolEntry {
@@ -31,37 +63,28 @@ pub struct ProtocolEntry {
   number: c_int 
 }
 
+impl FromNative for ProtocolEntry {
+  fn from_native(entry: *const protoent) -> ProtocolEntry {
+    ProtocolEntry {
+      name: extract_name(entry),
+      aliases: extract_aliases(entry),
+      number: extract_number(entry)
+    }
+  }
+}
+
 impl ProtocolEntry {
+  
   pub fn by_name(name: &str) -> Result<ProtocolEntry, String> {
     let entry = unsafe{ getprotobyname(name.to_c_str().as_ptr()) };
 
     if entry.is_null() {
       return Err(format!("unable to retrieve protocol by name: {}", name));
     }
-
-    let mut aliases: Vec<String> = Vec::new();
-    let mut current_alias = unsafe{ (*entry).p_aliases };
     
-    while !current_alias.is_null() {
-      let rust_string = 
-        unsafe{ CString::new(*current_alias, false).as_str().unwrap().to_string() };
-      
-      aliases.push(rust_string);
-
-      current_alias = unsafe{ current_alias.offset(1) }; 
-    }
-
     Ok( 
-      ProtocolEntry {
-        name: unsafe{ CString::new((*entry).p_name, false).as_str().unwrap().to_string() },
-        aliases: aliases,
-        number: unsafe{ (*entry).p_proto }
-      }
+      FromNative::from_native(entry) 
     )
-  }
-
-  pub fn default() -> ProtocolEntry {
-    ProtocolEntry{ name: String::new(), aliases: Vec::new(), number: 0 }
   }
 }
 
