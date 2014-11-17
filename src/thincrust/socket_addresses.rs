35b92f45116e1c0ec95eb35b63436dce2e06d27f
PhilLavoie <maidenphil@hotmail.com>
2014-11-17 18:41:45 -0500

    added ipproto constants

--- src/thincrust/socket_addresses.rs
+++ src/thincrust/socket_addresses.rs
@@ -8,11 +8,12 @@ use c::types::{sockaddr, sa_family_t, sockaddr_in, sockaddr_in6};
 type SocketAddressFamily = sa_family_t; //most likely u16
 type PortInt = u16;
 
+//Eventually support all socket address type but also provide a custom one, for a fallback mechanism.
+//It could hold a *void and a size_t.
 enum SocketAddress {
   Ipv4{ port: PortInt, address: [u8, ..4] },
-  Ipv6,
-  Unix,
-  Packet 
+  Ipv6{ port: PortInt, address: [u16, ..8], flowinfo: u32, scope_id: u32 },
+  Unix{ path: [u8, ..108] },
 }
 
 
